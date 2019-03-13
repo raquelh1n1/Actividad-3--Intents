5865352a2593bb53647da108ab99a9c77779fff6
raquelh1n1 <47800752+raquelh1n1@users.noreply.github.com>
2019-03-13 13:12:59 -0600

    Add files via upload

---
+++ main/java/RaquelH1N1/actividad4/SegundaActividad.java
@@ -0,0 +1,22 @@
+package RaquelH1N1.actividad4;
+
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SegundaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_actividad);
+
+        TextView tv = findViewById(R.id.tv);
+
+    }
+
+}
